b9308d7de03bb9de72dc309d388c7c811d9da87a
goranaviani <goranaviani@gmail.com>
2019-05-10 16:16:13 +0200

    exiting from while loop

--- stopwatch.py
+++ stopwatch.py
@@ -8,13 +8,15 @@ lapNum = 1
 
 try:
     while True:
-        input()  # press ENTER to do a new lap
+        userInput = input()  # press ENTER to do a new lap
         lapTime = round(time.time() - lastTime, 2)
         totalTime = round(time.time() - startTime, 2)
         print("Lap number {}, total time: {}, lap time: {}".format(lapNum, totalTime, lapTime))
         lapNum += 1
         lastTime = time.time()
-
+        if userInput.upper() == "X":
+            print("Good bye !")
+            break
 
 
 except KeyboardInterrupt:
