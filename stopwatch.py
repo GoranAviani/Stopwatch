46781bde56bc180f2c9ddf94e8ee62971473bb5e
goranaviani <goranaviani@gmail.com>
2019-05-10 16:13:49 +0200

    fix

--- stopwatch.py
+++ stopwatch.py
@@ -8,6 +8,7 @@ lapNum = 1
 
 try:
     while True:
+        input()  # press ENTER to do a new lap
         lapTime = round(time.time() - lastTime, 2)
         totalTime = round(time.time() - startTime, 2)
         print("Lap number {}, total time: {}, lap time: {}".format(lapNum, totalTime, lapTime))
