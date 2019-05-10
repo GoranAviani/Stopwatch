db3adc953c562a13def0db4930eac45c186ef8b0
goranaviani <goranaviani@gmail.com>
2019-05-10 16:12:55 +0200

    code

--- stopwatch.py
+++ stopwatch.py
@@ -6,3 +6,15 @@ startTime = time.time()
 lastTime = startTime
 lapNum = 1
 
+try:
+    while True:
+        lapTime = round(time.time() - lastTime, 2)
+        totalTime = round(time.time() - startTime, 2)
+        print("Lap number {}, total time: {}, lap time: {}".format(lapNum, totalTime, lapTime))
+        lapNum += 1
+        lastTime = time.time()
+
+
+
+except KeyboardInterrupt:
+    print("Good bye")
