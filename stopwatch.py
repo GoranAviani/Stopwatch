312abad07b92ab4978b52c07849dbb600580bd57
goranaviani <goranaviani@gmail.com>
2019-05-10 16:39:50 +0200

    if x is in the userInput the app will end, if not it is a new lap

--- stopwatch.py
+++ stopwatch.py
@@ -7,12 +7,14 @@ lastTime = startTime
 lapNum = 1
 while True:
     userInput = input()  # press ENTER to do a new lap
-    lapTime = round(time.time() - lastTime, 2)
-    totalTime = round(time.time() - startTime, 2)
-    print("Lap number {}, total time: {}, lap time: {}".format(lapNum, totalTime, lapTime))
-    lapNum += 1
-    lastTime = time.time()
     if userInput.upper() == "X":
         print("Good bye !")
         break
+    else:
+        lapTime = round(time.time() - lastTime, 2)
+        totalTime = round(time.time() - startTime, 2)
+        print("Lap number {}, total time: {}, lap time: {}".format(lapNum, totalTime, lapTime))
+        lapNum += 1
+        lastTime = time.time()
+
 
