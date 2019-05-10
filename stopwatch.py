89c923c5b7ee072999fafbd998bfc34a81f66d60
goranaviani <goranaviani@gmail.com>
2019-05-10 16:38:59 +0200

    stopwatch will run untill x

--- stopwatch.py
+++ stopwatch.py
@@ -5,19 +5,14 @@ print("Started")
 startTime = time.time()
 lastTime = startTime
 lapNum = 1
+while True:
+    userInput = input()  # press ENTER to do a new lap
+    lapTime = round(time.time() - lastTime, 2)
+    totalTime = round(time.time() - startTime, 2)
+    print("Lap number {}, total time: {}, lap time: {}".format(lapNum, totalTime, lapTime))
+    lapNum += 1
+    lastTime = time.time()
+    if userInput.upper() == "X":
+        print("Good bye !")
+        break
 
-try:
-    while True:
-        userInput = input()  # press ENTER to do a new lap
-        lapTime = round(time.time() - lastTime, 2)
-        totalTime = round(time.time() - startTime, 2)
-        print("Lap number {}, total time: {}, lap time: {}".format(lapNum, totalTime, lapTime))
-        lapNum += 1
-        lastTime = time.time()
-        if userInput.upper() == "X":
-            print("Good bye !")
-            break
-
-
-except KeyboardInterrupt:
-    print("Good bye")
